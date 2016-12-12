c8c3e440f38c36c64e1f374bc096f26202e0a7d5
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-12-12 19:17:38 +0200

    Update Bisection method to accept number of iterations and set default values #1

--- Bisection.m
+++ Bisection.m
@@ -3,11 +3,19 @@
 %   fnString -> Function in a string format
 %   x1 -> start of the interval
 %   x2 -> end of the interval
-%   tolerance -> step moved by the function
+%   iterations -> number of iterations default = 50
+%   tolerance -> step moved by the function default = 0.00001
 % @return
 %   root -> the value of the root of the function
 
-function root = Bisection(fnString, x1, x2, tolerance)
+function root = Bisection(fnString, x1, x2, iterations, tolerance)
+
+if nargin < 4
+    iterations = 50;
+    tolerance = 0.00001;
+elseif nargin < 5
+    tolerance = 0.00001;
+end
 
 syms fn(x)
 fn(x) = eval(fnString);
@@ -19,6 +27,7 @@ if fx1*fx2 > 0
     root = ('Error: The function has the same sign at the two given points');
 else
     n = ceil((log10(x2-x1)-log10(tolerance))/log10(2));
+    n = min(iterations, n);
     for i = 1:n
        root = (x1+x2)/2;
        fx = fn(root);
